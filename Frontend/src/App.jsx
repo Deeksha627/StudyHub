9c2bf836b9a9d1e6eeb3c11304b4f9d353bcc8a6
HemanshuTala <112183166+HemanshuTala@users.noreply.github.com>
2025-06-12 09:20:23 +0530

    commit with better folder structure and Tailwind css

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">Tailwind + Vite + React</h1>
+      <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 transition">
+        Test Button
+      </button>
+    </div>
+  )
+}
+
+export default App
